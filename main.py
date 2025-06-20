56e16db455063978eb5196ac1a89637af331a293
abrokecube <58220690+abrokecube@users.noreply.github.com>
2025-06-19 23:54:19 -0500

    surely

--- main.py
+++ main.py
@@ -495,7 +495,8 @@ class ChatClient(twitchio.Client):
         while True:
             sub_sub_items = {}
             for item, count in sub_items.items():
-                item_obj = ravenpy.get_item(item)
+                item_search = ravenpy.search_item(item, limit=1)
+                item_obj = ravenpy.get_item(item_search[0])
                 for ingredient in item_obj.craft_ingredients:
                     if not ingredient.item.craft_ingredients:
                         if ingredient.item.name not in resources:
