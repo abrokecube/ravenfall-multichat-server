8b5e2409006a4bfd372f6b23ec3cad631a433b6a
abrokecube <58220690+abrokecube@users.noreply.github.com>
2025-07-17 19:14:16 -0500

    icant bro

--- main.py
+++ main.py
@@ -1392,7 +1392,7 @@ class CommandServer:
                         channel_samples[char_data.channel_id] = []
                     channel_samples[char_data.channel_id].append(char_data.desync_s)
                     if char_data.last_update_time.timestamp() > desync_last_update_time:
-                        desync_last_update_time = char_data.last_update_time
+                        desync_last_update_time = char_data.last_update_time.timestamp()
 
             desync_per_channel_id: Dict[str, float] = {}
             for channel_id, samples in channel_samples.items():
